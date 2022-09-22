cb5a67615921869ebad42f47bcbacb3eae4148aa
Gautam Singh <gautam7109@gmail.com>
2022-09-23 02:23:34 +0530

    project added

--- src/App.js
+++ src/App.js
@@ -3,22 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+
+    <nav ClassName="navbar bg-light">
+      <div ClassName="container-fluid">
+        <span ClassName="navbar-brand mb-0 h1">Navbar</span>
+      </div>
+    </nav>
   );
 }
 
